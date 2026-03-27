a3c8a79b8adf3cfa3bbc37d13ef5c860ca134cb4
Matt <matt@Mac.attlocal.net>
2026-03-27 19:44:58 -0400

    updating posthog host

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -5,7 +5,7 @@ import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 
 const key = process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "";
-const host = process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com";
+const host = "https://us.i.posthog.com";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
